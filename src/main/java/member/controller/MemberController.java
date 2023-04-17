98363094b8aebf0d7e3ca208ae4d9bec263ffad1
노영선 <dudtjs303@yonsei.ac.kr>
2023-04-17 09:50:44 +0900

    컨트롤러명 변경

--- src/main/java/member/controller/MemberController.java
+++ src/main/java/member/controller/MemberController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController {
+public class MemberController {
 	
 	@RequestMapping(value = "myPage", method = RequestMethod.GET)
 	public String myPage() {
