ca564d4594a073b997cead96a6ebb56a9c062338
노영선 <121652746+Nyoungsun@users.noreply.github.com>
2023-04-17 18:33:19 +0900

    admin_main → adminMain

--- src/main/java/admin/controller/AdminController.java
+++ src/main/java/admin/controller/AdminController.java
@@ -1,6 +1,5 @@
 package admin.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AdminController {
 	
-	@RequestMapping(value = "admin_main", method = RequestMethod.GET)
-	public String admin_main() {
-		return "admin_main";
+	@RequestMapping(value = "adminMain", method = RequestMethod.GET)
+	public String adminMain() {
+		return "adminMain";
 	}
 	
 }
